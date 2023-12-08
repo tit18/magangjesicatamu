3c28852c2c8fcaf001d3be35dad184c91bc2b5d4
Raffi Joeta <raffijatirogo@gmail.com>
2023-12-08 17:18:48 +0700

    modify migrations

---
+++ migrations/20231208100650-modify_histori_tamu.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    return Promise.all([
+      queryInterface.addColumn(
+        'histori_tamus',
+        'no_wa_pengunjung',
+        {
+          type: Sequelize.STRING,
+          allowNull: true,
+        },
+      ),
+      ]);
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+  },
+
+  async down (queryInterface, Sequelize) {
+    return Promise.all([
+      queryInterface.removeColumn('histori_tamus', 'no_wa_pengunjung')
+    ]);
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  },
+};
